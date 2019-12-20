55a547e4ca30e97e52dc9c15e41f842b2ef248bc
amitnh <amitnh@gmail.com>
2019-12-20 17:20:59 +0200

    msgbroker a bit more

--- src/main/java/bgu/spl/mics/MessageBrokerImpl.java
+++ src/main/java/bgu/spl/mics/MessageBrokerImpl.java
@@ -14,9 +14,10 @@ public class MessageBrokerImpl implements MessageBroker {
 	 * Retrieves the single instance of this class.
 	 */
 	private static MessageBrokerImpl instance =new MessageBrokerImpl(); //makes the class singelton
-	private ConcurrentHashMap<Class,LinkedList<Subscriber>> subscribeEventMap = new ConcurrentHashMap<Class,LinkedList<Subscriber>>();
-	private ConcurrentHashMap<Class,Subscriber> subscribeBroadcastMap;
-	private ConcurrentHashMap<Subscriber,LinkedBlockingQueue<Message>> subscribersQueueMap;
+	private HashMap<Class<? extends Event>,LinkedList<Subscriber>> subscribeEventMap = new HashMap<Class<? extends Event>,LinkedList<Subscriber>>();
+	private HashMap<Class<? extends Broadcast>,LinkedList<Subscriber>> subscribeBroadcastMap = new HashMap<Class<? extends Broadcast>,LinkedList<Subscriber>>();
+	private ConcurrentHashMap<Subscriber,LinkedBlockingQueue<Message>> subscribersQueueMap = new ConcurrentHashMap<Subscriber,LinkedBlockingQueue<Message>>();
+	private ConcurrentHashMap<Event,Future> futureHashMap;
 
 
 	//private List<Pair<Class,List<Subscriber>>> subscribeEventList;
@@ -26,20 +27,25 @@ public class MessageBrokerImpl implements MessageBroker {
 	}
 	@Override
 	public <T> void subscribeEvent(Class<? extends Event<T>> type, Subscriber m) {
-
-		List<Subscriber> lst= subscribeEventMap.putIfAbsent(type, new LinkedList<>());// works atomically
-		lst.add(m);
+		synchronized (type)
+		{
+			if(!subscribeEventMap.containsKey(type))
+				subscribeEventMap.put(type,new LinkedList<Subscriber>());
+			subscribeEventMap.get(type).add(m);
+		}
 	}
-
 	@Override
 	public void subscribeBroadcast(Class<? extends Broadcast> type, Subscriber m) {
-		// TODO Auto-generated method stub
-
+		synchronized (type)
+		{
+			if(!subscribeBroadcastMap.containsKey(type))
+				subscribeBroadcastMap.put(type,new LinkedList<Subscriber>());
+			subscribeBroadcastMap.get(type).add(m);
+		}
 	}
 
 	@Override
 	public <T> void complete(Event<T> e, T result) {
-		// TODO Auto-generated method stub
 
 	}
 
@@ -52,25 +58,46 @@ public class MessageBrokerImpl implements MessageBroker {
 	
 	@Override
 	public <T> Future<T> sendEvent(Event<T> e) {
-		// TODO Auto-generated method stub
-		return null;
+		Subscriber m;
+		try {
+			synchronized (e.getClass()) { // to check
+				//check if not empty, or it is null
+				m = subscribeEventMap.get(e.getClass()).getFirst(); // brings the first subscriber in the event list
+				subscribeEventMap.get(e.getClass()).add(subscribeEventMap.get(e.getClass()).remove(0)); // moves the subscriber to the end of the list ?
+			}
+		}
+			catch( e){return null;}
+
+			Future<T> future = new Future<>();
+			//save hashmap of event-future ?
+			synchronized (m) {
+				subscribersQueueMap.get(m).add(e); //adds the Event to the subscriber Queue
+				m.notifyAll();
+			}
+		return future;
+
+
 	}
+
+
 
 	@Override
 	public void register(Subscriber m) {
-		// TODO Auto-generated method stub
-
+		// ConcurrentHashMap<Subscriber,LinkedBlockingQueue<Message>> subscribersQueueMap;
+		subscribersQueueMap.putIfAbsent(m,new LinkedBlockingQueue<Message>()); // works atomically
 	}
 
 	@Override
 	public void unregister(Subscriber m) {
-		// TODO Auto-generated method stub
-
+		subscribersQueueMap.remove(m);
+		//removes from everything .... dont forget
 	}
 
 	@Override
 	public Message awaitMessage(Subscriber m) throws InterruptedException {
-		// TODO Auto-generated method stub
+		synchronized () {// if there are not masseges
+			m.wait();
+		}
 		return null;
 	}
 
